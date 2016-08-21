c4d61e44226101f620a0af4771702ae25029e8d5
mitri4 <mitri4@bk.ru>
2016-08-21 23:31:13 +0300

    add yandex map test

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+let myMap;
+
+ymaps.ready(init);
+
+function init () {
+
+    myMap = new ymaps.Map('map', {
+
+        center: [55.76, 37.64],
+        zoom: 11,
+        controls: ['zoomControl', 'fullscreenControl']
+    });
+
+}
